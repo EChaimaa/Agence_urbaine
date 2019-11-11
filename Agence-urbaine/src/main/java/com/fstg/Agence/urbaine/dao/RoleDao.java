ec2249df7aecb54da266f14d540af892bba1bcfa
Moïse Gui <40016551+MoiseGui@users.noreply.github.com>
2019-11-11 20:26:22 +0100

    Project

--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/RoleDao.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/RoleDao.java
@@ -4,7 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.fstg.Agence.urbaine.bean.Role;
-import com.fstg.Agence.urbaine.bean.User;
 
 @Repository
 public interface RoleDao extends JpaRepository<Role, Long> {
