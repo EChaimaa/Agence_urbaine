a4edc3107c1d24f73777c67b21d431c49e7fb745
Faissal <Faissal@DESKTOP-KR0J6ND>
2019-11-25 16:29:52 +0100

    Project

--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/TypeProjetDao.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/TypeProjetDao.java
@@ -5,7 +5,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fstg.Agence.urbaine.bean.TypeProjet;
 
 public interface TypeProjetDao extends JpaRepository<TypeProjet, Long> {
-	public TypeProjet findByLibelle(String libelle);
 	
-
+	public TypeProjet findByLibelle(String libelle);
 }
