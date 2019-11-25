b5561671877168dc76357e0dcc9f553aa44a92a4
Moïse Gui <40016551+MoiseGui@users.noreply.github.com>
2019-11-25 16:44:32 +0100

    Project

--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/TypeCommissionDao.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/dao/TypeCommissionDao.java
@@ -7,5 +7,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.fstg.Agence.urbaine.bean.TypeCommission;
 
 public interface TypeCommissionDao extends JpaRepository<TypeCommission,Long> {
+	
 	public List<TypeCommission> findByLibelle(String libelle);
 }
