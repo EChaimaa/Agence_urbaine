3d5a35aa04306b43cd6c0fc12ca7525bd12df9f0
Chaimaa <Chaimaa@CHAIMAA-PC>
2019-12-16 18:37:01 +0100

    hii

--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/rest/TypeCommissionRest.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/rest/TypeCommissionRest.java
@@ -1,5 +1,32 @@
 package com.fstg.Agence.urbaine.rest;
 
-public class TypeCommissionRest {
+import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.fstg.Agence.urbaine.bean.TypeCommission;
+import com.fstg.Agence.urbaine.service.TypeCommissionService;
+
+@RestController
+@RequestMapping("/urbaine-api/client")
+public class TypeCommissionRest {
+	@Autowired
+	private TypeCommissionService typeCommissionService;
+
+	public TypeCommissionService getTypeCommissionService() {
+		return typeCommissionService;
+	}
+
+	public void setTypeCommissionService(TypeCommissionService typeCommissionService) {
+		this.typeCommissionService = typeCommissionService;
+	}
+	@GetMapping("/Libelle/{Libelle}")
+	public List<TypeCommission> findByLibelle(@PathVariable String Libelle) {
+		return typeCommissionService.findbylLibelle(Libelle);
+	}
+	
 }
