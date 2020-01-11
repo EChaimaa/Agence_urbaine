daa63378a2b22a49cc41ccaaba6b23720f425e0f
Chaimaa <Chaimaa@CHAIMAA-PC>
2020-01-11 17:53:32 +0100

    hiiiiiiiiiiiiiiiiiii

--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/rest/TypeCommissionRest.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/rest/TypeCommissionRest.java
@@ -1,24 +1,32 @@
 package com.fstg.Agence.urbaine.rest;
 
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.fstg.Agence.urbaine.bean.TypeCommission;
 import com.fstg.Agence.urbaine.service.TypeCommissionService;
 
 @RestController
 @RequestMapping("/urbaine-api/client")
 public class TypeCommissionRest {
 	@Autowired
-	private TypeCommissionService typeCommissionService;
+	 TypeCommissionService typeCommissionService;
 
-	public TypeCommissionService getTypeCommissionService() {
-		return typeCommissionService;
+	@GetMapping("/")
+	public List<TypeCommission> findAll() {
+		return typeCommissionService.findAll();
 	}
-
-	public void setTypeCommissionService(TypeCommissionService typeCommissionService) {
-		this.typeCommissionService = typeCommissionService;
+	
+	@PostMapping("/")
+	public void save(@RequestBody TypeCommission typeCommission) {
+		typeCommissionService.save(typeCommission);
 	}
 	
 }
--- Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/service/TypeCommissionService.java
+++ Agence-urbaine/src/main/java/com/fstg/Agence/urbaine/service/TypeCommissionService.java
@@ -5,5 +5,6 @@ import java.util.List;
 import com.fstg.Agence.urbaine.bean.TypeCommission;
 
 public interface TypeCommissionService {
-public List<TypeCommission> findAll();
+	public List<TypeCommission> findAll();
+	public void save(TypeCommission typeCommission);
 }
